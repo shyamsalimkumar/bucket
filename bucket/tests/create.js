8aa553360650829e4be96c10d9c596afd509d481
shyam <wackojacko@live.in>
2013-12-24 18:08:41 +0530

    Added dummy test for the create func. Added test and dev dependancies to the package.json.

--- bucket/tests/create.js
+++ bucket/tests/create.js
@@ -1,1 +1,13 @@
 // Tests for the create function
+var assert = require('assert'),
+    Bucket = require('../bin');
+
+describe('Bucket', function () {
+    var bucket;
+
+    describe('create()', function () {
+        it('should have "create" as _command', function () {
+            assert.equal(-1, [1,2,3].indexOf(5));
+        });
+    });
+});
